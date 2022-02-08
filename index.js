b5db2f4b155d977457d07fa7534125e8637d4130
Sanjaya609 <Sanjayakhatri5@gmail.com>
2022-02-08 15:53:00 +0545

    Marking as complete Done

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ async function getTodos(){
         url:ApiUrl
     }).catch((err)=>{console.log(err)})).data;
 
-    console.log(Todos);
+    //console.log(Todos);
     showtasks(Todos);
 };
 
 
 //To show data in list
-const lists= document.querySelector("#lecture-list ul");
+
 function showtasks(data){
+    const lists= document.querySelector("#lecture-list ul");
     const lisArray= [];
     let priority,color;
     data.forEach((todos)=>{
@@ -78,13 +79,120 @@ function showtasks(data){
 //for showing tasks initially 
 document.addEventListener('DOMContentLoaded',()=>{
     getTodos();
-})
-
-
-//To post data from form
-const form= document.querySelector("#lecture-add");
-form.addEventListener("submit", 
-async function (event) {
-    event.preventDefault();
     
-});
+    //To post data from form
+    const form= document.querySelector("#lecture-add");
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        //console.log("success");
+        let name=document.querySelector('#todo-task').value;
+        let priority=document.querySelector('#priority').value;
+        let description=document.querySelector('#description').value;
+
+        let setTodo={
+            name:name,
+            priority:priority,
+            description:description,
+            completed:false
+        };
+
+        setTodos(setTodo);
+
+        //function to post data
+        function setTodos(data){
+            axios({
+                method:"post",
+                url:ApiUrl,
+                data:data,
+            }).then((Response)=>{
+                alert("Task added Successfully");
+                getTodos();
+            }).catch((err)=>{console.log(err)});
+        }
+        document.querySelector('#todo-task').value="";
+        document.querySelector('#priority').value=0;
+        document.querySelector('#description').value="";
+    });
+
+
+    //To delete task
+    const lists = document.querySelector("#lecture-list ul");
+    lists.addEventListener("click", (event) => {
+      //console.log(event.target.classList)
+      if (event.target.classList[1] === "btn-danger") {
+        //console.log(event.target.parentElement.parentElement.id);
+
+        let target = event.target.parentElement.parentElement.id;
+        removeTodo(target);
+
+      } else if (event.target.classList[1] === "fa-trash-alt") {
+        let target = event.target.parentElement.parentElement.parentElement.id;
+        removeTodo(target);
+      }
+    });
+    //function to remove task
+    function removeTodo(id){
+        //console.log(id);
+
+        axios({
+            method:"delete",
+            url:`https://infodev-server.herokuapp.com/api/todos/${id}`
+        }).then((response)=>{
+            alert("Task has been deleted successfully");
+            getTodos();
+        }).catch((err)=>{console.log(err)});
+    }
+
+
+    //I tried doing this way but it crashed the server each time so i took help to fetch the id of button clicked
+    /* function deleteTask(){
+    let removeBtn=document.getElementsByClassName("btn btn-danger")
+    console.log(removeBtn);
+    console.log(removeBtn.length);
+    for (var i = 0; i < removeBtn.length; i++) {
+        removeBtn[i].addEventListener("click",(event)=>{
+            var id=event.target.parentElement.parentElement;
+            axios({
+                method: "delete",
+                url: `https://infodev-server.herokuapp.com/api/todos/${id}`,
+              })
+                .then((res) => {
+                  console.log("Todo deleted successfully");
+                  getTodos();
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            });
+        }
+    } */
+
+
+
+    //To mark as complete
+    lists.addEventListener("click", (event) => {
+        //console.log(event.target.classList)
+        if (event.target.classList[1] === "btn-success") {
+            //console.log(event.target.parentElement.parentElement);
+            var target = event.target.parentElement.parentElement;
+  
+        } else if (event.target.classList[1] === "fa-check") {
+            var target = event.target.parentElement.parentElement.parentElement;
+        }
+        completeTodo(target.id);
+        function completeTodo(id){
+            axios({
+                method:'put',
+                url:`https://infodev-server.herokuapp.com/api/todos/${id}`,
+                data:{
+                    completed:true
+                }
+            }).then((response)=>{
+                alert("Updated");
+                getTodos();
+        }).catch((err)=>{console.log(err);
+        })
+        }
+    });
+
+})
